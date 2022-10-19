33b5caa653e66d71101e9533dcd78e6455308385
Mukesh8056 <115688537+Mukesh8056@users.noreply.github.com>
2022-10-19 18:16:06 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,30 @@
+jsonData ={                     //for in loop
+    one: [11, 12, 13, 14, 15],
+    two: [21, 22, 23],
+    three: [31, 32]
+ }
+ 
+ for(var key in jsonData) {
+    for (var key1 in jsonData[key]) {
+        console.log(jsonData[key])
+    }
+ }
+
+ json ={                         //for of loop is not sutable in object
+    one: [1,2,3,4,5],
+    two: [6,7,8],
+    three: [10,20]
+ }
+ 
+ for(var key of json) {
+    for (var key1 of json[key]) {
+    
+        console.log(json[key])
+    }
+    }
+ 
+                                         //for loop regular 
+    var numbers = [ 10, 20, 30, 40, 50] 
+for (var i=0; i < numbers.length; i++) {
+   console.log(numbers[i])
+}
---
+++ script.js
@@ -0,0 +1,21 @@
+let resume=[{
+    "name":"Mukesh",
+    "age": "23",
+    "BOB":"18/06/1999",
+    "nativity":"india",
+    "religion":"hindu",
+
+},
+{
+    "degree":"B.E",
+    "COURSE":"ELECTRIC AND ELECTRONIC ENGINEER",
+    "school":"Bharathiyar hr sec school",
+    "college":"ST.Joseph institute of technology",
+    "location":"chennai",
+},
+{
+    "area of interest": "FULL STACK DEVELOPER",
+    "CERTIFICATE": "web developing in guvi", 
+
+}]
+console.log(resume)
